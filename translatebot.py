9d7a50f8e7c74072fd64f43edac8744f5aca718b
Timothy Asiimwe <atimothee@gmail.com>
2016-12-16 23:27:15 +0300

    setup verify webhook

--- translatebot.py
+++ translatebot.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request, abort
 
 app = Flask(__name__)
 
@@ -6,6 +7,15 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/webhook')
+def webhook():
+    challenge = request.args.get('hub.challenge')
+    token = request.args.get('hub.verify_token')
+    if token == '54321':
+        return challenge
+    abort(500)
 
 
 if __name__ == '__main__':
